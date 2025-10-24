b1ce7661357e492c454f0941471ecbef70e10948
jameszhao1021 <351917842@qq.com>
2025-10-25 02:34:20 +0800

    Built Skeleton

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+interface Activity  {
+    id: string;
+    title:string;
+    date: Date;
+    Description:string;
+    category:string;
+    IsCancelled: boolean;
+    city: string;
+    venue: string;
+    latitude: number;
+    longitude: number;
+  }
