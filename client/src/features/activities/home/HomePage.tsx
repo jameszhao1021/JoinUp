f4461e5e04dfde858089e151571c93810c7669e7
jameszhao1021 <351917842@qq.com>
2026-02-15 23:30:12 +0800

    Modified homepage

--- client/src/features/activities/home/HomePage.tsx
+++ client/src/features/activities/home/HomePage.tsx
@@ -13,6 +13,7 @@ export default function HomePage() {
     gap:6,
     alignItems:'center',
     alignContent:'center',
+    justifyContent: 'center',
     height:'100vh',
     backgroundImage:'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
    }}>
